aa70c2584520a7f348c6a66be822b26d52b09ce1
WuMenghao <33742731+WuMenghao@users.noreply.github.com>
2019-05-31 20:49:51 +0800

    Spring Cloud Config 配置

---
+++ cloud-routing/src/main/java/com/wmh/zuul/routing/CloudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.wmh.zuul.routing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudDemoApplication.class,args);
+    }
+
+}
