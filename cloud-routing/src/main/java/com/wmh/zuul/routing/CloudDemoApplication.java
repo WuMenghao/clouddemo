82003ea252eb584785b492d13668f5e93d0ff48d
WuMenghao <33742731+WuMenghao@users.noreply.github.com>
2019-06-01 10:11:18 +0800

    Spring Cloud zuul 配置

--- cloud-routing/src/main/java/com/wmh/zuul/routing/CloudDemoApplication.java
+++ cloud-routing/src/main/java/com/wmh/zuul/routing/CloudDemoApplication.java
@@ -2,7 +2,9 @@ package com.wmh.zuul.routing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class CloudDemoApplication {
 
